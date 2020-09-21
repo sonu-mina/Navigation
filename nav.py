af96d87e3ecf9c43531358b8cd2b50e69a637035
Sonu Mina <sonumina8644@gmail.com>
2020-09-22 01:43:26 +0530

    added_run

--- nav.py
+++ nav.py
@@ -3,10 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from heapq import heapify, heappush, heappop
 import time
-from queue import PriorityQueueYY
+from queue import PriorityQueue
+
+class Point:
+	'''
+		A point object that contains x & y co-ordinates
+		and connections with other points
+	'''
+	x, y, connections = None, None, set()
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.connections = set()
+
+	def __str__(self):
+		return '('+str(round(self.x, 2))+','+str(round(self.y, 2))+')'
+
+	def distance(self, point):
+		return ((self.x-point.x)**2 + (self.y-point.y)**2)**(1/2)
+		# return max(abs(self.x-point.x), abs(self.y-point.y))
+
+	def isValid(self, point, max_distance):
+		return self.distance(point)<max_distance and self.distance(point)>0
+
+
 
 class Map:
-    
 	
 	# Define Variables
 	points = []
---
+++ run.py
@@ -0,0 +1,26 @@
+from nav import Map
+
+def run():
+	n_points = int(input("No of points : ")) # 400
+	length = int(input("Enter length : ")) # 10
+	width = int(input("Enter width : ")) # 8
+
+	m_map = Map()
+
+	t_start = m_map.defineStart()
+
+	t_dest = m_map.defineDestination()
+
+	m_map.bakePath(n_points, length, width)
+	# m_map.showAllConnections(m_map)
+	# m_map.showStart()
+
+	start = m_map.findStart(t_start) # Index of starting point
+	dest = m_map.findDestination(t_dest) # Index of destination point
+	# dest = 0 # Index of destination point
+
+	path = m_map.getPathNew(start, dest)
+	m_map.showPath(path)
+
+
+run()
