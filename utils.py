aaf960c1a029ab4275e07c9d6e9c027ae3159f17
Sonu Mina <sonumina8644@gmail.com>
2020-09-22 00:42:22 +0530

    Created utilities

---
+++ utils.py
@@ -0,0 +1,21 @@
+class Point:
+	'''
+		A point object that contains x & y co-ordinates
+		and connections with other points
+	'''
+	x, y, connections = None, None, set()
+
+	def _init_(self, x, y):
+		self.x = x
+		self.y = y
+		self.connections = set()
+
+	def _str_(self):
+		return '('+str(round(self.x, 2))+','+str(round(self.y, 2))+')'
+
+	def distance(self, point):
+		return ((self.x-point.x)*2 + (self.y-point.y)2)*(1/2)
+		# return max(abs(self.x-point.x), abs(self.y-point.y))
+
+	def isValid(self, point, max_distance):
+		return self.distance(point)<max_distance and self.distance(point)>0
